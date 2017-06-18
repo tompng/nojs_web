e353377e59b57a41cee66718bcdc244386e2bb29
tompng <tomoyapenguin@gmail.com>
2017-06-18 16:11:30 +0900

    clock sample indent

--- samples/sleep.rb
+++ samples/sleep.rb
@@ -1,8 +1,10 @@
 get '/clock1' do
   stream do |out|
-    out.puts '<html><head></head><body>'
+    out.puts '<html>'
+    out.puts '  <head><title>clock1</title></head>'
+    out.puts '  <body>'
     loop do
-      out.puts "<p>#{Time.now.strftime '%Y/%m/%d %H:%M:%S'}</p>"
+      out.puts "    <p>#{Time.now.strftime '%Y/%m/%d %H:%M:%S'}</p>"
       sleep 1
     end
   end
@@ -10,11 +12,15 @@ end
 
 get '/clock2' do
   stream do |out|
-    out.puts '<html><head></head><body>'
-    out.puts '<style>body{font-size:48px;}</style>'
+    out.puts '<html>'
+    out.puts '  <head>'
+    out.puts '    <title>clock2</title>'
+    out.puts '    <style>body{font-size:48px;}</style>'
+    out.puts '  </head>'
+    out.puts '  <body>'
     (0..Float::INFINITY).each do |i|
-      out.puts "<center id='d#{i}'>#{Time.now.strftime '<small>%Y/%m/%d</small><br>%H:%M:%S'}</center>"
-      out.puts "<style>#d#{i-1}{display:none}</style>"
+      out.puts "    <center id='d#{i}'>#{Time.now.strftime '<small>%Y/%m/%d</small><br>%H:%M:%S'}</center>"
+      out.puts "    <style>#d#{i-1}{display:none}</style>"
       sleep 1
     end
   end

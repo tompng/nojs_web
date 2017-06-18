348a277458494d39d7980b77013eabce2987c9d6
tompng <tomoyapenguin@gmail.com>
2017-06-18 17:00:03 +0900

    use jst

--- samples/sleep.rb
+++ samples/sleep.rb
@@ -4,7 +4,8 @@ get '/clock1' do
     out.puts '  <head><title>clock1</title></head>'
     out.puts '  <body>'
     loop do
-      out.puts "    <p>#{Time.now.strftime '%Y/%m/%d %H:%M:%S'}</p>"
+      now = Time.now.localtime 9*60*60
+      out.puts "    <p>#{now.strftime '%Y/%m/%d %H:%M:%S'}</p>"
       sleep 1
     end
   end
@@ -19,7 +20,8 @@ get '/clock2' do
     out.puts '  </head>'
     out.puts '  <body>'
     (0..Float::INFINITY).each do |i|
-      out.puts "    <center id='d#{i}'>#{Time.now.strftime '<small>%Y/%m/%d</small><br>%H:%M:%S'}</center>"
+      now = Time.now.localtime 9*60*60
+      out.puts "    <center id='d#{i}'>#{now.strftime '<small>%Y/%m/%d</small><br>%H:%M:%S'}</center>"
       out.puts "    <style>#d#{i-1}{display:none}</style>"
       sleep 1
     end
